05724173a6c27c0d2045c830d77b7d204a7ee8c6
han408 <58838776+han408@users.noreply.github.com>
2019-12-23 00:51:04 +0900

    Add files via upload

--- demo/src/main/java/com/example/Application.java
+++ demo/src/main/java/com/example/Application.java
@@ -1,13 +1,7 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
