a7c87d8c19b6bcf0f9bc71f3fcdeade11f656fee
han408 <58838776+han408@users.noreply.github.com>
2019-12-23 00:49:28 +0900

    Add files via upload

--- demo/src/main/java/com/example/repository/HoufincinstRepository.java
+++ demo/src/main/java/com/example/repository/HoufincinstRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.model.Houfincinst;
-import com.example.model.Houfincsupllist; 
 
 @Repository
 public interface HoufincinstRepository extends JpaRepository<Houfincinst, String> {
